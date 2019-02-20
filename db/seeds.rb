490bb9f219ef40119fa3f49a43983183f19776f7
Selcuk Karakus <selchuck.karakus@gmail.com>
2019-02-20 15:17:52 +0000

    added 5 valid restaurant records in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Hings Canteen',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number:        '0208 543354'
+  },
+  {
+    name:         'Franco Manca',
+    address:      '56A Al capone High St, Roma E11 6PQ',
+    category:  'italian',
+    phone_number:        '0208 543355'
+  },
+  {
+    name:         'The Bonzai Tree',
+    address:      '76A Toyatasanai, Tokyo E17 6PR',
+    category:  'japanese',
+    phone_number:        '0208 543356'
+  },
+  {
+    name:         'Cafe Rouge',
+    address:      '46A  Monroue St, Paris E10 6PS',
+    category:  'french',
+    phone_number:        '0208 543357'
+  },
+  {
+    name:         'The Brussels Mussels',
+    address:      '16A Fooden High St, Brussels E1 5QR',
+    category:  'belgian',
+    phone_number:        '0208 543358'
+  }]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
